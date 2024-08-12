4d867784242097bf0a4f6bb83cd7b48fe2b209bc
rahul26122005 <rahulr26122005@gmail.com>
2024-08-12 20:50:13 +0530

    add the pkg config

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
